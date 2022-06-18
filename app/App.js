376b9d666fbbe026fa99c0e1c0d7b91890c040fc
Dibash Thapa <dibashthapa55@gmail.com>
2022-06-18 13:07:24 +0545

    ignored

--- app/App.js
+++ app/App.js
@@ -1,25 +1,21 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
-
  *
  * @format
  * @flow strict-local
  */
 
 import React from 'react';
-import Geolocation from 'react-native-geolocation-service';
+import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  PermissionsAndroid,
   StyleSheet,
   Text,
   useColorScheme,
   View,
-  Button,
-  Alert,
 } from 'react-native';
 
 import {
@@ -29,9 +25,8 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import {useEffect, useState} from 'react';
 
-const Section = ({children, title}) => {
+const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -57,41 +52,8 @@ const Section = ({children, title}) => {
   );
 };
 
-const App = () => {
+const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
-  const [hasLocationPermission, setHasLocationPermission] = useState(false);
-
-  const requestLocationAccess = async () => {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        title: 'Emerygency App',
-        message: 'Example App access to your location ',
-      },
-    );
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      setHasLocationPermission(true);
-    } else {
-      setHasLocationPermission(false);
-    }
-  };
-
-  const onPress = () => {
-    if (!hasLocationPermission) {
-      Alert.alert("You don't have location permission enabled");
-    }
-    Geolocation.getCurrentPosition((success, err) => {
-      if (success) {
-        console.log('current location is', success);
-      }
-      if (err) {
-        console.warn('Error occured', err);
-      }
-    });
-  };
-  useEffect(() => {
-    requestLocationAccess();
-  }, []);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -109,13 +71,8 @@ const App = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Hello <Text style={styles.highlight}>App.js</Text> to change this
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
-            <Button
-              title="Emergency Button"
-              color="#f194ff"
-              onPress={onPress}
-            />
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
@@ -124,7 +81,7 @@ const App = () => {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to ReloadInstructions what to do next:
+            Read the docs to discover what to do next:
           </Section>
           <LearnMoreLinks />
         </View>
